eec2f1b0687aa5bcd931747b62b714592e7d891a
Emily <emily.j.langley@gmail.com>
2024-03-14 09:16:21 -0700

    added a check to add melon to cart and increment count if melon is in cart

--- shoppingsite.py
+++ shoppingsite.py
@@ -77,6 +77,7 @@ def add_to_cart(melon_id):
         
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
+    cart[melon_id] = cart.get(melon_id, 0) + 1
     
     # - flash a success message
     # - redirect the user to the cart page
